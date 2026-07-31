8fb4f80a7cfb97ad7be3b98b8bd1dce90d7a8b0a
Riccardo Merolla <riccardo.merolla@hey.com>
2026-07-31 04:53:25 +0200

    add-kebabcase: Implement kebabCase in src/strcase.js

--- test/strcase.test.js
+++ test/strcase.test.js
@@ -1,6 +1,6 @@
 import assert from "node:assert/strict"
 import { test } from "node:test"
-import { camelCase, words } from "../src/strcase.js"
+import { camelCase, kebabCase, words } from "../src/strcase.js"
 
 test("words splits on separators and camel boundaries", () => {
   assert.deepEqual(words("foo_bar-baz qux"), ["foo", "bar", "baz", "qux"])
@@ -12,4 +12,13 @@ test("camelCase joins words with capitalization", () => {
   assert.equal(camelCase("foo bar baz"), "fooBarBaz")
   assert.equal(camelCase("foo_bar"), "fooBar")
   assert.equal(camelCase(""), "")
+})
+
+test("kebabCase joins words with hyphens", () => {
+  assert.equal(kebabCase("foo bar"), "foo-bar")
+  assert.equal(kebabCase("fooBarBaz"), "foo-bar-baz")
+  assert.equal(kebabCase("foo_bar"), "foo-bar")
+  assert.equal(kebabCase("foo"), "foo")
+  assert.equal(kebabCase("foo-bar"), "foo-bar")
+  assert.equal(kebabCase(""), "")
 })
